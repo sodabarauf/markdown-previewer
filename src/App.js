bb2b502628365e5de3136a34ca042aeed0f63475
sodabarauf <sodabarauf4@gmail.com>
2024-08-21 19:49:42 +0430

    added some function to app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-
+import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+
+  {
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+  }
+  \`\`\`
+
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
+
   return (
     <div className="App">
-     
+      <textarea id="editor" onChange={(event) => {setText(event.target.value);
+
+      }}
+      value={text}
+      ></textarea>
+      <div id="preview" dangerouslySetInnerHTML={{
+        __html: marked(text),
+      }}></div>
     </div>
   );
 }
